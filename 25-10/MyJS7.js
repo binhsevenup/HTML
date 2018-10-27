008800825c60b7ff042b8bfae2f5d8b4a44c6575
binhsevenup <43373110+binhsevenup@users.noreply.github.com>
2018-10-27 09:55:02 +0700

    Update MyJS7.js

--- 25-10/MyJS7.js
+++ 25-10/MyJS7.js
@@ -1,19 +1,20 @@
-// input.addEventListener("keyup", function(event) {
-//     event.preventDefault();
-//     if (event.keyCode === 13) {
-//         document.getElementById("btnSearch").onclick();
-//     }
-// });
-
 document.getElementById("btnSearch").onclick = function () {
+    var keyword = document.getElementById("key-word").value;
+    searchTubes(keyword);
+};
 
-    var keyword = document.getElementById("keyword").value;
+document.getElementById("keyword").onkeypress = function (e) {
+    if (e.which === 13 || e.keyCode === 13) {
+        keyword = document.getElementById('keyword').value;
+        searchTubes(keyword);
+    }
+
+};
+function searchTubes(keyword) {
     var YOUTUBE_API = "https://content.googleapis.com/youtube/v3/search?q=" + keyword + "&type=video&maxResults=9&part=snippet&key=AIzaSyAwUjk3CwtXCiB_W6Xi0colfOKPgm90hHc";
-
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(xhttp.responseText);
             var jsObject = JSON.parse(xhttp.responseText);
             var content = '';
             for (var i = 0; i < jsObject.items.length; i++) {
@@ -31,5 +32,5 @@ document.getElementById("btnSearch").onclick = function () {
     };
     xhttp.open("GET", YOUTUBE_API, true);
     xhttp.send();
-}
 
+}
